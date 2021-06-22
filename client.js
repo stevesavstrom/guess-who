1ea66e2da3cdc16556e74cbaeb2bb05b64c36fc4
Steve Savstrom <steve.savstrom@gmail.com>
2021-06-21 19:25:33 -0500

    initial commit - gameboard displayed

--- client.js
+++ client.js
@@ -1,1 +1,15 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(readyNow);
+
+function readyNow(){
+loadImage();
+}
+
+function loadImage() {
+	for(let person of people)
+	$('#gameboard').append(`<img src="https://github.com/${person.githubUsername}.png?size=250">`)
+}
+
+
+
--- data.js
+++ data.js
@@ -27,4 +27,40 @@ var people = [
     name: 'Chris',
     githubUsername: 'christopher-black'
   },
+  {
+    name: 'Steve',
+    githubUsername: 'stevesavstrom'
+  },
+  {
+    name: 'Duy',
+    githubUsername: 'DuyDao-Dev'
+  },
+  {
+    name: 'John',
+    githubUsername: 'johnidso'
+  },
+  {
+    name: 'Matthew',
+    githubUsername: 'matthewbuoc'
+  },
+  {
+    name: 'Meghan',
+    githubUsername: 'Queerty'
+  },
+  {
+    name: 'Tim',
+    githubUsername: 'tadugan'
+  },
+  {
+    name: 'Steve',
+    githubUsername: 'stevesavstrom'
+  },
+  {
+    name: 'Steve',
+    githubUsername: 'stevesavstrom'
+  },
+  {
+    name: 'Steve',
+    githubUsername: 'stevesavstrom'
+  },
 ];
