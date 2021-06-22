6d71c610e3c839df965f8805b89c8bfb50a221f1
Steve Savstrom <steve.savstrom@gmail.com>
2021-06-21 22:30:17 -0500

    added comments and removed unneccessary css selector

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 console.log('Here are all the available people:', people);
 
+// Needed to declare globally to help with clickPhoto() comparison
 let randNum = -1;
 
 $(document).ready(readyNow);
@@ -7,35 +8,37 @@ $(document).ready(readyNow);
 function readyNow(){
 loadImages();
 randomPerson();
+// Click event listener
 $("#gameboard").on("click", "div", clickPhoto);
-// click event listener
-}
+} // end readyNow
 
 // Load images on DOM when page loads
 function loadImages() {
+	// loop through data.js array and append each photo by Github username to DOM
 	for(let person of people)
 	$('#gameboard').append(`<div class="grid" id="${person.name}"><img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}"></div>`)
-}
+} // end loadImages
 
 // Random number function (provided)
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
-}
+} // end randomNumber
 
 // Generate random person and append into #randomName
 function randomPerson(){
 	randNum = randomNumber(0, people.length - 1);
 	$('#randomName').append(people[randNum].name);
 	console.log(randNum);
-}
+} // end randomPerson
 
 // Guess and check by clicking on the photo
 function clickPhoto(){
+	// comparing id name to random person
 	if(this.id === people[randNum].name){
 		alert("Nice job! You found me!");
 	} else {
 		alert("Try again!");
 	}
-}
+} // end clickPhoto
 
 
